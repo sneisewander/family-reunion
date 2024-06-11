c5e9f41c4e7613681835feef8f49e7dfce089c2c
sneisewander <sneisewander@raynor.com>
2024-06-11 15:34:18 -0500

    many changes

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
 console.log('hello world')
+
+$("#back-to-top").on("click", (e) => {
+    console.log('hello world')
+    $('#main-content').animate({
+        scrollTop: $("#main-content").offset().top
+    },'slow')
+})
