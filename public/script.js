fe0d5088f091d683dea2fa947f414a951928a435
sneisewander <sneisewander@raynor.com>
2024-06-12 13:52:00 -0500

    many changes

--- public/script.js
+++ public/script.js
@@ -1,8 +1,17 @@
-console.log('hello world')
-
-$("#back-to-top").on("click", (e) => {
-    console.log('hello world')
+$("#back-to-top").on("click", () => {
     $('#main-content').animate({
         scrollTop: $("#main-content").offset().top
     },'slow')
+})
+
+$(".m3-nav-drawer .text-button").on("click", (e) => {
+    $('#main-content').stop(true,false).animate({
+        scrollTop: $(`#main-content #${e.target.id}`).offset().top
+        + $('#main-content').scrollTop()
+        - $('#main-content').offset().top
+    },'slow')
+})
+
+$("button").on("animationend", (e) => {
+    $(e.target).blur()
 })
