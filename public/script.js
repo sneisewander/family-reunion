b06b57f49522fd5297779a9133130dbc1e1e9a32
sneisewander <sneisewander@raynor.com>
2024-06-20 13:33:30 -0500

    accesibility and color

--- public/script.js
+++ public/script.js
@@ -6,6 +6,8 @@ $("#back-to-top").on("click", () => {
 
 $(".m3-nav-drawer .text-button").on("click", (e) => {
     $('#main-content').stop(true,false).animate({
+        // Finds the distance between the top and the target, then the distance between the top and the current position, then scrolls the difference between those two.
+        // Also substracts 14 pixels to adjust for padding.
         scrollTop: $(`#main-content #${e.target.id}`).offset().top
         + $('#main-content').scrollTop()
         - $('#main-content').offset().top
@@ -14,5 +16,51 @@ $(".m3-nav-drawer .text-button").on("click", (e) => {
 })
 
 $("button").on("animationend", (e) => {
-    $(e.target).blur()
+    $(e.target).blur() // blur method unfocuses an element. this makes the animation repeatable on a single element multiple times in succession. 
+})
+
+//Settings
+
+let colorMode = 'light'
+let contrastModes = ['', '-medium-contrast', '-high-contrast']
+let contrastIndex = 0
+
+try {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        $('#toggle-color-mode span').text('dark_mode')
+        colorMode = 'dark'
+        $('#logo').css('content', 'url(assets/RAYNORWWWHITE.png)')
+    }
+    if (window.matchMedia && window.matchMedia('(prefers-contrast: less)').matches) contrastIndex = 1
+    else if (window.matchMedia && window.matchMedia('(prefers-contrast: less)').matches) contrastIndex = 2
+} catch (error) {
+    console.warn('Media preferences could not be fetched.')
+}
+
+
+$('body').removeClass().addClass(`${colorMode}${contrastModes[contrastIndex]}`)
+
+$("#toggle-color-mode").on("click", () => {
+    console.log($('#toggle-color-mode span').text())
+    switch ($('#toggle-color-mode span').text()) {
+        case "light_mode":
+            $('#toggle-color-mode span').text('dark_mode')
+            colorMode = 'dark'
+            $('#logo').css('content', 'url(assets/RAYNORWWWHITE.png)')
+            break
+        case "dark_mode":
+            $('#toggle-color-mode span').text('light_mode')
+            colorMode = 'light'
+            $('#logo').css('content', 'url(assets/RAYNORWW1COLOR.png)')
+            break
+    }
+
+    $('body').removeClass().addClass(`${colorMode}${contrastModes[contrastIndex]}`)
+})
+
+$("#toggle-contrast-mode").on("click", () => {
+    console.log($('#toggle-color-mode span').text())
+    if (contrastIndex >= contrastModes.length - 1) contrastIndex = 0
+    else contrastIndex++
+    $('body').removeClass().addClass(`${colorMode}${contrastModes[contrastIndex]}`)
 })
